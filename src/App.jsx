93ecc1a72ebcbc2a2b687210e1e8e50413eb63f1
MohammadSaifulIslam <saifmdislam231@gmail.com>
2023-04-20 12:03:21 +0600

    seassion storage and popup modal added

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,29 @@ import Header from './components/Header';
 import { Outlet, useLoaderData } from 'react-router-dom';
 import Footer from './components/Footer';
 import toast, { Toaster } from 'react-hot-toast';
+import Modal from './components/Modal';
 
 export const CartContext = createContext([]);
 export const ProductContex = createContext([])
 
 const App = () => {
+  const [isOpen, setIsOpen ] = useState(false)
   const [products ,cartArr] = useLoaderData()
   const [cart, setCart] = useState(cartArr)
+
+  const cartAlert = sessionStorage.getItem('alert')
+  if(cart.length > 0 && cartAlert !== 'true'){
+    setIsOpen(true)
+    sessionStorage.setItem('alert', true)
+  }
+
   return (
     <CartContext.Provider value={[cart, setCart]}>
       <ProductContex.Provider value={products}>
         <Header></Header>
         <Outlet></Outlet>
         <Footer></Footer>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen}></Modal>
         <Toaster />
       </ProductContex.Provider>
     </CartContext.Provider>
